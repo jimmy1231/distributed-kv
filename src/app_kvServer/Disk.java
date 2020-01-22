c874b958dd288fe5f2cc3553dd73a71c628a69cc
Patricia <patsmrkt@gmail.com>
2020-01-22 11:27:25 -0500

    initial implementation of disk storage

--- src/app_kvServer/Disk.java
+++ src/app_kvServer/Disk.java
@@ -1,23 +1,97 @@
 package app_kvServer;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Disk {
-    private static final String KV_FILE = "";
+    private static final String KV_STORE_FILE = "kv_store.txt";
+    private static ReadWriteLock RW_LOCK = new ReentrantReadWriteLock();
 
     public Disk() {
         // do nothing for now
     }
 
     /**
+     * Given key and value, formats the pair into the following
+     * format to be stored in the kv_store.txt file:
+     *      <key> <value>
+     *  Key + value separated by a space
+     *  NOTE: if there are any design changes to the persistent
+     *  storage format, only this function should change
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    private static String formatKVPair(String key, String value) {
+        return key + " " + value;
+    }
+
+    /**
+     * Creates kv_store.txt file if DNE
+     */
+    private static void createKVStoreFile() {
+        Lock writeLock = RW_LOCK.writeLock();
+        writeLock.lock();
+        try {
+            File file = new File(KV_STORE_FILE);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (Exception ex) {
+
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+
+    /**
      * Given a key:
      *  - Returns value of KV pair if key exists in file
      *  - Returns null if key DNE
      *
+     *  Searches through all of the lines in the KV_STORE_FILE
+     *  until it finds the requested key or reaches EOF.
+     *
      * @param key
      * @return value of KV pair
      */
-    public static String getKV(String key) { return null; }
+    public static String getKV(String key) {
+        // Create KV Store File if DNE
+        createKVStoreFile();
+
+        Lock read_lock = RW_LOCK.readLock();
+        read_lock.lock();
+
+        String value = null;
+        try {
+            FileReader fr = new FileReader(KV_STORE_FILE);
+            BufferedReader reader = new BufferedReader(fr);
+            String line;
+            String []kvPair;
+            while ((line = reader.readLine()) != null) {
+                // split line into [ key, value ]
+                kvPair = line.split(" ", 2);
+
+                // found a match
+                if (kvPair[0].equals(key)) {
+                    value = kvPair[1];
+                    break;
+                }
+            }
+
+        } catch (Exception ex) {
+            // do nothing for now - write to ERROR log
+        } finally {
+            read_lock.unlock();
+        }
+        return value;
+    }
 
     /**
      * Given a key, value pair:
@@ -27,7 +101,61 @@ public class Disk {
      * @param key
      * @param value
      */
-    public static void putKV(String key, String value) { return; }
+    public static void putKV(String key, String value) {
+        // Create KV Store File if DNE
+        createKVStoreFile();
+
+        Lock writeLock = RW_LOCK.writeLock();
+        writeLock.lock();
+        List<String> kvPairs = new ArrayList<String>();
+        boolean exists = false;
+        try {
+            FileReader fr = new FileReader(KV_STORE_FILE);
+            BufferedReader reader = new BufferedReader(fr);
+            String line;
+            String []kvPair;
+            while ((line = reader.readLine()) != null) {
+                // split line into [ key, value ]
+                kvPair = line.split(" ", 2);
+
+                // found a match
+                if (kvPair[0].equals(key)) {
+                    exists = true;
+                    line = formatKVPair(key, value);
+                }
+                kvPairs.add(line);
+            }
+            reader.close();
+            fr.close();
+
+            // Write all lines to file if exists
+            // If does not exist, just append to the file
+            FileWriter fw;
+            BufferedWriter  writer;
+            if (exists) {
+                fw = new FileWriter(KV_STORE_FILE);
+                writer = new BufferedWriter(fw);
+                for (String pair : kvPairs) {
+                    writer.write(pair);
+                    writer.newLine();
+                }
+            } else {
+                fw = new FileWriter(KV_STORE_FILE, true);
+                writer = new BufferedWriter(fw);
+                writer.write(formatKVPair(key, value));
+            }
+
+            writer.close();
+            fw.close();
+
+        } catch (Exception ex) {
+            // do nothing for now
+            System.out.println("AAH ERROR");
+        } finally {
+            writeLock.unlock();
+        }
+
+    }
 
     /**
      * Given a key:
@@ -43,5 +171,16 @@ public class Disk {
      * Clears all contents of persistent storage
      */
     public static void clearStorage() { }
+
+    /* For debugging */
+    public static void main(String[] args) {
+        putKV("key", "value");
+        System.out.println(getKV("key"));
+        putKV("key", "value_2");
+        System.out.println(getKV("key"));
+        putKV("hello", "world how are you 38108301");
+        System.out.println(getKV("hello"));
+        System.out.println(getKV("goodbye"));
+    }
 
 }
