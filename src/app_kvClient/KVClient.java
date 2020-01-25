8eb7ceb2fffba0fef6e1618e927775d1e7e5a558
Ann Yun <jurue96@gmail.com>
2020-01-25 09:35:53 -0500

    Change client side main from CLI to KVClient. Connection Test all passes

--- src/app_kvClient/KVClient.java
+++ src/app_kvClient/KVClient.java
@@ -1,7 +1,12 @@
 package app_kvClient;
 
+import client.CLI;
 import client.KVCommInterface;
 import client.KVStore;
+import logger.LogSetup;
+import org.apache.log4j.Level;
+
+import java.io.IOException;
 
 public class KVClient implements IKVClient {
     private KVCommInterface KVStoreInstance;
@@ -9,9 +14,9 @@ public class KVClient implements IKVClient {
 
     @Override
     public void newConnection(String hostname, int port) throws Exception{
-        KVStoreInstance = new KVStore(hostname, port);
-        running = true;
-        KVStoreInstance.connect();
+            KVStoreInstance = new KVStore(hostname, port);
+            running = true;
+            KVStoreInstance.connect();
     }
 
     @Override
@@ -21,5 +26,17 @@ public class KVClient implements IKVClient {
 
     public boolean isRunning() {
         return running;
+    }
+
+    public static void main(String[] args) {
+        try {
+            new LogSetup("logs/client.log", Level.OFF);
+            client.CLI app = new CLI();
+            app.run();
+        } catch (IOException e) {
+            System.out.println("Error! Unable to initialize logger!");
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
