2391bf20a898a5a39f006988ed42eb0917fc5218
Jimmy Li <dfjimmy.li@gmail.com>
2020-04-02 20:24:28 -0400

    minor change

--- src/app_kvServer/dsmr/impl/WordFreqMapReduce.java
+++ src/app_kvServer/dsmr/impl/WordFreqMapReduce.java
@@ -4,10 +4,16 @@ import app_kvServer.dsmr.MapInput;
 import app_kvServer.dsmr.MapReduce;
 import app_kvServer.dsmr.ReduceInput;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.function.BiConsumer;
 
 public class WordFreqMapReduce extends MapReduce {
+    private static final Set<String> EXCLUDE_WORD_LIST = new HashSet<>(Arrays.asList(
+        "the", "of", "for", "in", "you", "a"
+    ));
     public WordFreqMapReduce(BiConsumer<String, String> Emit) {
         // Define emitter
         super(Emit);
@@ -18,9 +24,13 @@ public class WordFreqMapReduce extends MapReduce {
         final String text = input.getValue();
         final String[] split = text.split(" ");
 
+        String token;
         int i;
         for (i=0; i<split.length; i++) {
-            Emit.accept(split[i], "1");
+            token = split[i];
+            if (!EXCLUDE_WORD_LIST.contains(token)) {
+                Emit.accept(split[i], "1");
+            }
         }
     }
 
