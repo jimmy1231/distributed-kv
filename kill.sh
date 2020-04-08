5a3d245e4155a0b2d9844d8dd85a62704ca5baa8
Jimmy Li <dfjimmy.li@gmail.com>
2020-04-08 16:04:40 -0400

    refining kill.sh

--- kill.sh
+++ kill.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-for PORT_NUM in 50007 50008 50009 50010 50011 50012 50013 50014 50015 50016 50017 50018 50019 50020 50021
+input="src/app_kvECS/config/ecs.config"
+while IFS=' ' read -r serverName ip port
 do
-    echo "killing ${PORT_NUM}"
-    kill $(ps aux | grep ${PORT_NUM} | awk '{print $2}')
-done
+    echo "Server_Name: $serverName, IP: $ip, Port: $port"
+    ssh -n ${port} nohup kill -9 $(ps aux | grep ${port} | awk '{print $2}')
+done < "$input"
 
 rm -f ~/kv_store* && rm -f ~/replica* && rm -f ~/logs/server*.log
+#for PORT_NUM in 50007 50008 50009 50010 50011 50012 50013 50014 50015 50016 50017 50018 50019 50020 50021
+#do
+#    echo "killing ${PORT_NUM}"
+#    kill $(ps aux | grep ${PORT_NUM} | awk '{print $2}')
+#done
+#
+#rm -f ~/kv_store* && rm -f ~/replica* && rm -f ~/logs/server*.log
